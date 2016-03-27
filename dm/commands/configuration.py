e92fb0d274331bc3852e3e307d79cee1074b1f7c
pronto <vitaly.pronto@gmail.com>
2016-03-27 13:59:49 +0300

    added saving to disk

--- dm/commands/configuration.py
+++ dm/commands/configuration.py
@@ -2,14 +2,18 @@
 
 
 from .base import Base
-
+import json
 
 class Configuration(Base):
 
     def run(self):
         # /clusters/{cluster}/config
         result = self._send("/ui/api/clusters/" + self.options.get('--cluster') + "/config")
+
         print(result)
+
+        with open(self.options.get('--file'), 'w') as outfile:
+            json.dump(data, outfile)
 
 
 
