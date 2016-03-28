6daff0f79373f25c8268d2d491e7d34b7e06879b
pronto <vitaly.pronto@gmail.com>
2016-03-28 16:37:03 +0300

    fixed printing

--- dm/commands/containers.py
+++ dm/commands/containers.py
@@ -1,6 +1,6 @@
 """list of containers."""
 
-
+import json
 from .base import Base
 
 
@@ -11,5 +11,5 @@ class Containers(Base):
         result = self._send("/ui/api/clusters/" + self.options.get('--cluster') + "/containers")
 
         keys = ['id', 'name', 'node', 'cluster', 'image', 'ports', 'status']
-        self._print(keys, result)
+        self._print(keys, json.loads(result))
 
