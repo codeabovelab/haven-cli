30d31b2f5f3f4435e938d05205f17c98d8994dc7
pronto <vitaly.pronto@gmail.com>
2016-03-28 18:24:02 +0300

    fixed job_update

--- dm/commands/job_update.py
+++ dm/commands/job_update.py
@@ -5,8 +5,8 @@ from .base import Base
 class JobUpdate(Base):
     def run(self):
         # /containers/update
-        data = json.dumps({'service': self.options.get('--service'), 'version': self.options.get('--version'),
-                           'strategy': self.options.get('--version')})
+        data = json.dumps({'service': self.options.get('--image'), 'version': self.options.get('--to_version'),
+                           'strategy': self.options.get('--strategy'), 'percentage': '100'})
         result = self._send(
             "/ui/api/clusters/" + self.options.get('--cluster') + "/containers/update",
             method='POST', data=data)
