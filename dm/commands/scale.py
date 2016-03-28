a62fc22d04e46ad800ec3c6d13cf33c2f7d459a9
pronto <vitaly.pronto@gmail.com>
2016-03-28 15:50:28 +0300

    fixed scale

--- dm/commands/scale.py
+++ dm/commands/scale.py
@@ -7,5 +7,5 @@ class Scale(Base):
     def run(self):
         # /clusters/{cluster}/containers/{id}/scale
         self._send("/ui/api/clusters/" + self.options.get('--cluster') + "/containers/" + self.options.get(
-            '--cluster') + "/scale")
+            '--id') + "/scale")
         print("ok")
