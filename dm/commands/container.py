82e00778e0943a0ecd62785bf281f551772ded86
pronto <vitaly.pronto@gmail.com>
2016-07-27 11:31:33 +0300

    added operations by name

--- dm/commands/container.py
+++ dm/commands/container.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""usage: dm container (start|stop|restart|create|scale)  (--cluster=<cluster> --name=<name> | --id=<id>) --server=<server> --port=<port> --login=<login> --password=<password> [--columns=<id,name,node,cluster,image,ports,status>] [--help] [--verbose=<level>]
+"""usage: dm container (start|stop|restart|rm|create|scale)  (--cluster=<cluster> --name=<name> | --id=<id>) [--tag=<tag> --image=<image> --containerName=<name> ]
+            --server=<server> --port=<port> --login=<login> --password=<password> [--columns=<id,name,node,cluster,image,ports,status>] [--help] [--verbose=<level>]
 
 Options:
   -h --help                         Show this screen.
@@ -13,10 +14,14 @@ Options:
   -n --name=<name>                  Container name
   --id=<id>                         Container id
   --columns=<column1,column2>       List of columns [default: id,name,node,cluster,image,ports,status]
+  --tag=<tag>                       Image tag
+  --image=<image>                   Image name
+  --containerName=<name>            Container name
 
 Commands:
   start                             Start container by --id or --cluster and --name
   stop                              Stop container by --id or --cluster and --name
+  rm                                Remove container by --id or --cluster and --name
   restart                           Restart container by --id or --cluster and --name
   scale                             Scale container by --id or --cluster and --name
   create                            Create container by --id
@@ -54,30 +59,56 @@ class Containers(Base):
         create = self.options.get('create')
         if create:
             self.__create()
+        rm = self.options.get('rm')
+        if rm:
+            self.__rm()
 
     def __stop(self):
         # post /ui/api/containers/{id}/stop
-        result = self._send("/ui/api/containers/" + self.options.get('--id') + "/stop", method='POST')
+        container_id = self.__calc_id()
+        result = self._send("/ui/api/containers/" + container_id + "/stop", method='POST')
         print(result)
 
     def __start(self):
         # post /ui/api/containers/{id}/start
-        result = self._send("/ui/api/containers/" + self.options.get('--id') + "/start", method='POST')
+        container_id = self.__calc_id()
+        result = self._send("/ui/api/containers/" + container_id + "/start", method='POST')
+        print(result)
+
+    def __rm(self):
+        # post /ui/api/containers/{id}/remove
+        container_id = self.__calc_id()
+        result = self._send("/ui/api/containers/" + container_id + "/remove", method='POST')
         print(result)
 
     def __scale(self):
         # post /ui/api/containers/{id}/scale
-        result = self._send("/ui/api/containers/" + self.options.get('--id') + "/scale", method='POST')
+        container_id = self.__calc_id()
+        result = self._send("/ui/api/containers/" + container_id + "/scale", method='POST')
         print(result)
 
     def __restart(self):
         # /clusters/{cluster}/containers/{id}/restart
-        result = self._send("/ui/api/containers/" + self.options.get('--id') + "/restart", method='POST')
+        container_id = self.__calc_id()
+        result = self._send("/ui/api/containers/" + container_id + "/restart", method='POST')
         print(result)
 
     def __create(self):
-        # /clusters/{cluster}/containers/create
-        data = json.dumps({'tag': self.options.get('--tag'), 'image': self.options.get('--image')})
-        result = self._send("/ui/api/clusters/" + self.options.get('--cluster') + "/containers/create",
-            method='POST', data=data)
+        # /containers/create
+        data = json.dumps({'cluster': self.options.get('--cluster'),
+                           'tag': self.options.get('--tag'),
+                           'containerName': self.options.get('--containerName'),
+                           'image': self.options.get('--image')})
+        result = self._send("/ui/api/containers/create",
+                            method='POST', data=data)
         print(result)
+
+    def __calc_id(self):
+        container_id = self.options.get('--id')
+        if container_id:
+            return container_id
+        #/containers/{cluster}/{name}
+        result = self._send("/ui/api/containers/" + self.options.get('--cluster') + "/" + self.options.get('--name'),
+                            method='GET')
+        data = json.loads(result)
+        return data.get('id')
